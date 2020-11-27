214b881d1d467f81c42992f20012aebb6513a8ab
Muhammad Adil Abbas <sagheer.hussain@intelvue.com>
2020-11-28 03:05:48 +0500

    Content Update

---
+++ resources/views/terms-conditions.blade.php
@@ -0,0 +1,117 @@
+
+@extends('layouts.front')
+
+@section('content')
+
+<div class="terms-section">
+<section class="py-5">
+   <div class="container">
+      <h1 class="mb-5">Terms and Conditions for Qurbani Kiston Pay</h1>
+      <h3 class="mb-3">Introduction</h3>
+      <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, Qurbani Kiston Py accessible at <strong><a href="#">qkponline.com</a></strong>.</p>
+      <p>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions.</p> 
+      <p class="mb-0">Minors or people below 18 years old are not allowed to use this Website.</p>
+   </div>
+</section>
+
+<section class="py-5 bg_white">
+ <div class="container">
+      <h3 class="mb-3">Intellectual Property Rights</h3>
+      <p>Other than the content you own, under these Terms, Qurbani Kiston Pay and/or its licensors own all the intellectual property rights and materials contained in this Website.</p>
+      <p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
+      
+      <h3 class="mt-4">Restrictions</h3>
+   <p>You are specifically restricted from all of the following:</p>
+   <ul class="pl-4">
+      <li>publishing any Website material in any other media;</li>
+      <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+      <li>publicly performing and/or showing any Website material;</li>
+      <li>using this Website in any way that is or may be damaging to this Website;</li>
+      <li>using this Website in any way that impacts user access to this Website;</li>
+      <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
+      <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
+      <li>using this Website to engage in any advertising or marketing.</li>
+   </ul>
+   <p>Certain areas of this Website are restricted from being access by you and Qurbani Kiston Pay may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.<p>
+   
+<h3 class="mt-4">Your Content</h3>
+<p>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant Qurbani Kiston Pay a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
+<p>Your Content must be your own and must not be invading any third-party’s rights. Qurbani Kiston Pay reserves the right to remove any of Your Content from this Website at any time without notice.</p>
+
+<h3 class="mt-4">Your Privacy</h3>
+<p>Please read Privacy Policy.</p>
+
+<h3 class="mt-4">No warranties</h3>
+<p>This Website is provided "as is," with all faults, and Qurbani Kiston Pay express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
+
+<h3 class="mt-4">Limitation of liability</h3>
+<p>In no event shall Qurbani Kiston Pay, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  Qurbani Kiston Pay, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
+
+<h3 class="mt-4">Indemnification</h3>
+<p>You hereby indemnify to the fullest extent Qurbani Kiston Pay from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
+
+<h3 class="mt-4">Severability</h3>
+<p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
+
+<h3 class="mt-4">Variation of Terms</h3>
+<p>Qurbani Kiston Pay is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
+
+<h3 class="mt-4">Assignment</h3>
+<p>The Qurbani Kiston Pay is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+
+<h3 class="mt-4">Entire Agreement</h3>
+<p>These Terms constitute the entire agreement between Qurbani Kiston Pay and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
+
+<h3 class="mt-4">Governing Law & Jurisdiction</h3>
+<p>These Terms will be governed by and interpreted in accordance with the laws of the state of Islamic Republic of Pakistan, and you submit to the non-exclusive jurisdiction of the state and federal courts located in Pakistan for the resolution of any disputes.</p>
+
+</div>
+</section>
+
+   <!-- footer -->
+   <section class="footer-section">
+   <div class="container">
+      <div class="row justify-content-around text-center align-items-center">
+         <div class="col-sm-2 text-left">
+            <div class="footerlogo">
+                  <img src="/images/footerlogo.svg" alt="">
+            </div>
+         </div>
+         <div class="col-sm-6 copyright">
+            <span>© Copyrights 2020 QurbaniKistoonPay. All Rights Reserved</span>
+            <span><a href="#"><strong>Privacy Policy</strong></a> - <a href="#"><strong>Terms & Conditions</strong></a></span>
+         </div>
+         <div class="col-sm-3 social-icon">
+            <div class="text-right">
+               <a href="#">
+                  <i class="icon-qkp-facebook"></i>
+               </a>
+               <a href="#">
+                  <i class="icon-qkp-twitter"></i>
+               </a>
+               <a href="#">
+                  <i class="icon-qkp-youtube-play"></i>
+               </a>
+               <a href="#">
+                  <i class="icon-qkp-instagram"></i>
+               </a>
+               <a href="#">
+                  <i class="icon-qkp-snapchat"></i>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+</div>
+   <!-- footer end -->
+
+
+
+   <script src="/js/jquery-3.5.1.min.js"></script>
+   <script src="/js/popper.min.js"></script>
+   <script src="/js/bootstrap.min.js"></script>
+   <script src="/js/slick.js"></script>
+
+   @endsection
+   
