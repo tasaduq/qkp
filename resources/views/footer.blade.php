339c54841248dc6033434c6be44f247f194ad275
Muhammad Adil Abbas <sagheer.hussain@intelvue.com>
2020-11-29 05:47:11 +0500

    Footer Links Updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -9,7 +9,7 @@
           </div>
           <div class="col-sm-6 copyright">
              <span>© Copyrights 2020 QurbaniKistoonPay. All Rights Reserved</span>
-             <span><a href="#"><strong>Privacy Policy</strong></a> - <a href="#"><strong>Terms & Conditions</strong></a></span>
+             <span><a href="privacy-policy"><strong>Privacy Policy</strong></a> - <a href="terms-conditions"><strong>Terms & Conditions</strong></a></span>
           </div>
           <div class="col-sm-3 social-icon">
              <div class="text-right">
