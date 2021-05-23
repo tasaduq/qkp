239bd1a20580a34141eaecfef898008d3b343b0e
Tasaduq <taxaduq@gmail.com>
2021-05-23 18:16:32 +0500

    maintenance view

--- src/resources/views/maintenance.blade.php
+++ src/resources/views/maintenance.blade.php
@@ -1,1 +1,21 @@
-Under maintenance
+@extends('layouts.front')
+
+@section('content')
+
+<div class="container text-center d-flex align-items-center justify-content-center vh-100">
+    <div>
+       <a href="#"><img width="150" src="/images/logo.svg" alt=""></a><br />
+       <img class="pt-5 pb-0" width="250" src="/images/mentainance.png" alt="">
+       <h1 class="mt-5" style="color:#ffffff;">WEBSITE IS UNDER MAINTENANCE</h1>
+       <h3 class="mt-4" style="color:#ffffff;">We will be back shortly</h3>
+    </div>
+ </div>
+
+@endsection
+
+
+
+<style>
+    header{display:none !important; }
+    body{background-image: url("/images/bg.png"); background-attachment: fixed; background-size: cover;}
+ </style>
